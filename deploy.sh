1bfd241bd16265b4cc22b509ee6683400d93efc9
mingrammer <k239507@gmail.com>
2018-02-18 10:54:04 +0900

    Commit changes to hugo branch as well when deploying updates

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,14 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+msg="Rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+# Commit changes to hugo branch
+git commit -m "$msg"
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
@@ -10,11 +18,7 @@ cd public
 # Add changes to git.
 git add .
 
-# Commit changes.
-msg="Rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
+# Commit changes to master branch
 git commit -m "$msg"
 
 # Push source and build repos.
